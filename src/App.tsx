8d106a49608723dc48a88e2719c88021504690df
John Okyere <112992966+mhiskall282@users.noreply.github.com>
2024-12-29 03:57:16 +0000

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,49 @@
 import React from 'react';
-import { Header } from './components/layout/Header';
-import { Hero } from './components/home/Hero';
-import { Services } from './components/home/Services';
-import { About } from './components/home/About';
-import { Executives } from './components/home/Executives';
-import { Contact } from './components/home/Contact';
-import { Donate } from './components/home/Donate';
-import { Footer } from './components/layout/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import ProductGrid from './components/ProductGrid';
+import ProductDetails from './components/shop/ProductDetails';
+import Cart from './components/shop/Cart';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Footer from './components/Footer';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import AdminDashboard from './components/admin/AdminDashboard';
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <Header />
-      <main>
-        <Hero />
-        <Services />
-        <About />
-        <Executives />
-        <Contact />
-        <Donate />
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <CartProvider>
+          <div className="min-h-screen bg-white flex flex-col">
+            <Header />
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={
+                  <>
+                    <Hero />
+                    <ProductGrid />
+                  </>
+                } />
+                <Route path="/product/:id" element={<ProductDetails />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/admin" element={<AdminDashboard />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </CartProvider>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
+
 export default App;
